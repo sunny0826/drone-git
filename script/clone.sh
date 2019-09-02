404bc96adc547f99e64ff0490e3abda1404df53c
guoxudong <guoxudong@keking.cn>
2019-09-02 10:07:24 +0800

    add clone.sh

--- script/clone.sh
+++ script/clone.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-clone(){
-    git clone ${PLUGIN_CONFIG_PATH} ${PLUGIN_OUT}
-}
-
 echo "+ clone ${PLUGIN_CONFIG_PATH} to ${PLUGIN_OUT}"
 
+git clone ${PLUGIN_CONFIG_PATH} ${PLUGIN_OUT}
 #echo "configPkg: ${PLUGIN_OUT}" > env.yaml
