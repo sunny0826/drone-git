65a972a988f67bf615b03e74a24ae3d01e13e553
guoxudong <guoxudong@keking.cn>
2019-09-02 11:55:51 +0800

    add clone.sh

--- script/clone.sh
+++ script/clone.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-git config --global user.name "${DRONE_COMMIT_AUTHOR_NAME}"
-
 echo "+ clone ${PLUGIN_CONFIG_PATH} to ${PLUGIN_OUT}"
 
-git clone ${PLUGIN_CONFIG_PATH} ${PLUGIN_OUT}
+mkdir ${PLUGIN_OUT}
+cd ${PLUGIN_OUT}
+git init
+git remote add origin ${PLUGIN_CONFIG_PATH}
+git pull origin master
+
+#git clone ${PLUGIN_CONFIG_PATH} ${PLUGIN_OUT}
 
 /bin/drone-git
 #echo "configPkg: ${PLUGIN_OUT}" > env.yaml
