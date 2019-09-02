16a59089d908c56e125bf3bdd7cbee2ba857d677
guoxudong <guoxudong@keking.cn>
2019-09-02 12:24:36 +0800

    add clone.sh

--- script/clone.sh
+++ script/clone.sh
@@ -5,6 +5,8 @@ echo "+ clone ${PLUGIN_CONFIG_PATH} to ${PLUGIN_OUT}"
 mkdir ${PLUGIN_OUT}
 cd ${PLUGIN_OUT}
 git init
+git config --global user.name "guoxudong"
+git config --global user.email guoxudong@keking.cn
 git remote add origin ${PLUGIN_CONFIG_PATH}
 git pull origin master
 
